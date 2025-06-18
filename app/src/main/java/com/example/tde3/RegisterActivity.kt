245a2490c6ba7df2a2fb2f6e15fd0d05cdb0a83c
Yasmim Silva <yasmimsilvahh00@gmail.com>
2025-06-18 14:35:48 -0300

    feat: navigation pages

--- app/src/main/java/com/example/tde3/RegisterActivity.kt
+++ app/src/main/java/com/example/tde3/RegisterActivity.kt
@@ -3,33 +3,32 @@ package com.example.tde3
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlin.jvm.java
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnLogin = findViewById<Button>(R.id.btn_login)
-        val btnRegister = findViewById<Button>(R.id.btn_register)
-
-        btnRegister.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
+        val back = findViewById<ImageView>(R.id.imageView2)
+        back.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        btnLogin.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
+        val BtnRegister = findViewById<Button>(R.id.button)
+        BtnRegister.setOnClickListener {
+            val intent = Intent(this, HomemActivity::class.java)
             startActivity(intent)
         }
     }
